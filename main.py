a1c319d2fad242e794194634994ab3c32d0fbd1f
manel269 <83067825+manel269@users.noreply.github.com>
2022-08-15 11:24:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi iport FastAPI
 from  scraper import scrape
+#Enter your facebook's email id and password here 
+	email = ""
+	password = ""
+	login(email,password)
+	scrape()
+	driver.close()
+	        
 
 app = FastAPI()
 @app.get("/{list}")
 async def read_item():
-  return
+  	scrape()
+	  driver.close()
